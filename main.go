0cdd992ffb2d068a2fbaddf6567e62942f0d66c7
ludwig125 <18366858+ludwig125@users.noreply.github.com>
2019-08-17 06:14:50 +0900

    init trade-derby

--- main.go
+++ main.go
@@ -7,14 +7,24 @@ import (
 	"net/http"
 	"os"
 	"runtime"
+	"syscall"
+
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 var (
-	number  = flag.Int("number", 12345, "env number.")
-	envData = "default"
-	user    = "noset"
-	pass    = "noset"
+	number    = flag.Int("number", 12345, "env number.")
+	debug     = flag.Bool("debug", false, "Use Debug Mode?")
+	debugUser = flag.String("debuguser", "", "Debug User")
+	user      = "noset"
+	pass      = "noset"
 )
+
+type stockInfo struct {
+	Code   string
+	Name   string
+	Status string
+}
 
 func mustGetenv(k string) string {
 	v := os.Getenv(k)
@@ -26,8 +36,30 @@ func mustGetenv(k string) string {
 }
 
 func init() {
-	user = mustGetenv("APPUSER")
-	pass = mustGetenv("APPPASS")
+	flag.Parse()
+
+	log.Printf("use debug mode?: %t", *debug)
+	if *debug {
+		// debug modeの場合はdebuguserをオプションで指定し、passwordを入力する
+		u := *debugUser
+		if u == "" {
+			log.Fatal("debuguser noset. if you use debug=true, set debuguser")
+		}
+		user = u
+		log.Println("set debuguser:", u)
+
+		fmt.Print("Password: ")
+		p, err := terminal.ReadPassword(int(syscall.Stdin))
+		if err != nil {
+			log.Fatal("Failed to read password", err)
+		}
+		pass = string(p)
+		log.Println("set debugpass")
+	} else {
+		// GKEから実行する場合は環境変数から取得する
+		user = mustGetenv("APPUSER")
+		pass = mustGetenv("APPPASS")
+	}
 }
 
 func main() {
@@ -37,16 +69,9 @@ func main() {
 		port = fromEnv
 	}
 
-	if fromEnv := os.Getenv("ENVDATA"); fromEnv != "" {
-		envData = fromEnv
-	}
-	log.Printf("this is ENVDATA '%s'", envData)
-
-	log.Printf("this is USER '%s'", user)
-	log.Printf("this is PASS '%s'", pass)
-
 	server := http.NewServeMux()
 	server.HandleFunc("/", indexHandler)
+	server.HandleFunc("/tradederby", tradeDerby)
 
 	// start the web server on port and accept requests
 	log.Printf("Server listening on port: %s", port)
@@ -61,8 +86,17 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hostname: %s\n", host)
 	fmt.Fprintf(w, "cpu: %d\n", runtime.NumCPU())
 	fmt.Fprintf(w, "GOMAXPROCS: %d\n", runtime.GOMAXPROCS(0))
+}
 
-	flag.Parse()
-	fmt.Fprintf(w, "ENV NUMBER: %d\n", *number)
-	fmt.Fprintf(w, "ENV DATA: %s\n", envData)
+func tradeDerby(w http.ResponseWriter, r *http.Request) {
+	html, err := fetchStockDocFromWebPage(user, pass)
+	if err != nil {
+		log.Fatalf("Failed to fetchStockDocFromWebPage, %v", err)
+	}
+
+	stockInfos, err := fetchStockInfo(html)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(stockInfos)
 }
